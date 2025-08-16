78de2fa817cf126ecbd8f51d335e3e4379819685
soham313131 <sohamdang313131@gmail.com>
2025-08-16 23:14:23 +0530

    a

--- src/api.js
+++ src/api.js
@@ -5,5 +5,14 @@ const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
   withCredentials: true,
 });
+API.interceptors.request.use((req) => {
+  // Check if user info (with the token) exists in local storage
+  if (localStorage.getItem('userInfo')) {
+    // If it exists, add the 'Authorization' header to the request
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('userInfo')).token}`;
+  }
+  // Return the request so it can be sent on to the server
+  return req;
+});
 
 export default API;
